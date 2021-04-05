2f0e8cd208ab6ff6cf98d51fb706f6cd5a308f35
Hamza <hamzaazman6776@gmail.com>
2021-04-05 21:53:56 +0300

    Project created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'aes';
+
+  plainText: string;
+  encryptText: string;
+  encPassword: string;
+  decPassword: string;
+  conversionEncryptOutput: string;
+  conversionDecryptOutput: string;
+
+  constructor() {}
+  
+  convertText(conversion: string) {
+    if (conversion == 'encrypt') {
+      this.conversionEncryptOutput = CryptoJS.AES.encrypt(
+        this.plainText.trim(),
+        this.encPassword.trim()
+      ).toString();
+    } else {
+      this.conversionDecryptOutput = CryptoJS.AES.decrypt(
+        this.encryptText.trim(),
+        this.decPassword.trim()
+      ).toString(CryptoJS.enc.Utf8);
+    }
+  }
 }
